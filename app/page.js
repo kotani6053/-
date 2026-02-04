435b4cb5c6726e8b900bc2ae63a6d5f9626457b3
kotani6053 <yoshioka@e-kotani.co.jp>
2026-02-04 13:37:21 +0900

    Update page.js

--- app/page.js
+++ app/page.js
@@ -9,22 +9,26 @@ export default async function Home() {
   const data = await getData();
 
   return (
-    <div style={{
-      height: "100vh",
-      background: "#000",
-      color: "#fff",
-      display: "flex",
-      flexDirection: "column",
-      justifyContent: "center",
-      alignItems: "center"
-    }}>
+    <div
+      style={{
+        height: "100vh",
+        background: "#000",
+        color: "#fff",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
       <h1 style={{ fontSize: "48px" }}>会議室 使用状況</h1>
       <div style={{ fontSize: "32px", lineHeight: "1.8" }}>
         <p>使用者：{data.user}</p>
         <p>目的　：{data.purpose}</p>
         <p>時間　：{data.time}</p>
       </div>
-      <a href="/edit" style={{ marginTop: 40, color: "#aaa" }}>変更</a>
+      <a href="/edit" style={{ marginTop: 40, color: "#aaa" }}>
+        変更
+      </a>
     </div>
   );
 }
